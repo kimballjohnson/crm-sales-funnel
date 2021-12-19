b7bda62eaa57aa1523ff33576bdb0cadcc7957fe
Noam Lubofsky <noamlubofsky@gmail.com>
2021-12-19 18:32:40 -0500

    refix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
-
   resources :prospects
   resources :companies
   resources :tests
-
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-
-get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-
 end
