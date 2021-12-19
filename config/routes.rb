221f85e8f4cd5e9be1aecd5e7b8e6aef4571fe5a
Noam Lubofsky <noamlubofsky@gmail.com>
2021-12-19 18:29:12 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
 
   resources :prospects
   resources :companies
   resources :tests
 
-  end
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+end
