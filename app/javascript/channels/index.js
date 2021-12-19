de0e44c552bdbbd5d9e0e03a6d4cdd4d48fa372c
Noam Lubofsky <noamlubofsky@gmail.com>
2021-12-19 11:26:59 -0500

    Commit from heroku-deploy.py for deployment

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,13 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__vclab, '../client/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__vclab + '/../client/build/index.html'))
+})
