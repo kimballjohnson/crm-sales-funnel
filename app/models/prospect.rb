f9bd81d6b7c9f6fb4c953303d03a45373d9938d9
Noam Lubofsky <noamlubofsky@gmail.com>
2021-12-19 19:21:21 -0500

    fixed validations

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -3,6 +3,6 @@ class Prospect < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :stage, presence: true
+  # validates :stage, presence: true
   # validates :probability, presence: true, length: {minimum: 0, maximum: 100}
 end
