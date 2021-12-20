d88758231efc98c1314d362a826ea9fc54299ee3
Noam Lubofsky <noamlubofsky@gmail.com>
2021-12-19 19:14:54 -0500

    test

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,15 +1,1 @@
-// Load all the channels within this directory and all subdirectories.
-// Channel files must be named *_channel.js.
-
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
-
-const path = require('path')
-
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__vclab, '../client/build')))
-
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__vclab + '/../client/build/index.html'))
-})
+app.use('/post', postRoutes); if (process.env.NODE_ENV === 'production') {   app.use(express.static(path.join(__vclab, "client/build")));   app.get("*", (_, res) => {     res.sendFile(path.join(__vclab, "client/build", "index.html"));   }); }
